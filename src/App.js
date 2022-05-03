28a7a285f77580ca4f9af4c29486e2315e67d25b
linzhiyu <leo@rismit.io>
2022-05-03 16:09:41 +0800

    simple-example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useEffect, useState} from "react";
+import {ethers, Signer} from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json"
+import {use} from "chai";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("");
+    const [contract, setContract] = useState("");
+
+    const getData = async () => {
+        const data = await contract.greet();
+        setData(data);
+    }
+
+    const updateData = async () => {
+        const transaction = await contract.setGreeting(data);
+        await transaction.wait();
+        getData();
+    }
+
+    const initConnection = async () => {
+        if (typeof window.ethereum !== "undefined") {
+            await window.ethereum.request({method: "eth_requestAccounts"});
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+
+            setContract(
+                new ethers.Contract(
+                    "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+                    Greeter.abi,
+                    signer
+                )
+            );
+        } else {
+            console.log("please install metamask.")
+        }
+    };
+
+    useEffect(() => {
+        initConnection();
+    }, [])
+
+    return (
+        <div className="App">
+            <button onClick={getData}>getdata</button>
+            <button onClick={updateData}>set data</button>
+
+            <input
+                onChange={(e)=> setData(e.target.value)}
+                placeholder="new greeting"
+            />
+
+            <p>{data}</p>
+        </div>
+    );
 }
 
 export default App;
